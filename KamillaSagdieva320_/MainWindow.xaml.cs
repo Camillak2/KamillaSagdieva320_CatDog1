e3326d1028c410ae397584ed7b0340eb42d5dd4e
Камилла Сагдиева <ksagdieva2@gmail.com>
2023-10-26 09:41:03 +0300

    Добавила страницу со входом с разделенными ролями.

--- KamillaSagdieva320_/MainWindow.xaml.cs
+++ KamillaSagdieva320_/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KamillaSagdieva320_.Pages;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace KamillaSagdieva320_
         public MainWindow()
         {
             InitializeComponent();
+            NaFr.NavigationService.Navigate(new AuthorizationPage());
         }
     }
 }
--- KamillaSagdieva320_/Pages/AuthorizationPage.xaml.cs
+++ KamillaSagdieva320_/Pages/AuthorizationPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KamillaSagdieva320_.DB;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,29 @@ namespace KamillaSagdieva320_.Pages
     /// </summary>
     public partial class AuthorizationPage : Page
     {
+        public static List<Owner> owners { get; set; }
         public AuthorizationPage()
         {
             InitializeComponent();
+        }
+
+        private void EnterBTN_Click(object sender, RoutedEventArgs e)
+        {
+            string login = LoginTB.Text.Trim();
+            string password = PasswordTB.Password.Trim();
+
+            Owner owner = new Owner();
+            owners = new List<Owner>(DBConnection.catDogEntities.Owner.ToList());
+            Owner currentUser = owners.FirstOrDefault(i => i.Login == login && i.Password == password);
+            if (currentUser != null)
+            {
+                if (currentUser.ID == 1)
+                    NavigationService.Navigate(new KittyPage());
+                else if (currentUser.ID == 2)
+                    NavigationService.Navigate(new DogPage());
+            }
+            else
+                MessageBox.Show("Неверный логин или пароль. Попробуйте снова.");
         }
     }
 }
