dd469e10517a9d99a75924efa2cfcaa523c8d407
Камилла Сагдиева <ksagdieva2@gmail.com>
2023-10-26 08:50:04 +0300

    Подключила базу данных.

---
+++ KamillaSagdieva320_/DB/DBConnection.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KamillaSagdieva320_.DB
+{
+    internal class DBConnection
+    {
+        public static KamillaSagdieva320_CatDog1Entities catDogEntities = new KamillaSagdieva320_CatDog1Entities();
+    }
+}
